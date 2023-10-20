666947bda4377eb991e8d7cab8b08986f2360e13
K-irwin8 <Aqlo984@gmail.com>
2023-10-20 18:40:41 +0900

    IIDD cycle that works. Only using prompts

--- IIDDcycle.py
+++ IIDDcycle.py
@@ -7,14 +7,14 @@ openai.api_key = api_key
 
 from llama_index import VectorStoreIndex, SimpleDirectoryReader
 
-documents = SimpleDirectoryReader('/Users/markirwin/llama_index/examples/paul_graham_essay/data').load_data()
+documents = SimpleDirectoryReader('/Users/markirwin/llama_index/examples/paul_graham_essay/data_input').load_data()
 index = VectorStoreIndex.from_documents(documents)
 
 
 query_engine = index.as_query_engine()
 
 # Identify the goals
-end_goal_question="What do you think are the goals the author was trying to achive? Output in a list"
+end_goal_question="What do you think are the goals the author was trying to achive?"
 end_goal_response = query_engine.query(end_goal_question)
 
 # Identify the key insights etc
@@ -34,10 +34,17 @@ action_question="Based on the decisions he made, what actions did the author tak
 action_response = query_engine.query(action_question)
 
 # Synthesize
-synthesize_question=""
+with open("/Users/markirwin/llama_index/examples/paul_graham_essay/data_output/paul_graham_output.txt", "w") as file:
+    file.write(str(end_goal_response) + str(information_response) + str(issues_response) + str(decision_response) + str(action_response))
+    
+documents = SimpleDirectoryReader("/Users/markirwin/llama_index/examples/paul_graham_essay/data_output").load_data()
+index = VectorStoreIndex.from_documents(documents)
+query_engine = index.as_query_engine()
+
+synthesize_question="Output this text in a structered format with 5 parts. The first: On the goals the author had. Second: On the information the author used. Third: On the issues the author asked. Fourth: On the decisions the authos made. Fifth: On the actions the author took."
 synthesize_response = query_engine.query(synthesize_question)
 
-print(_response)
+print(synthesize_response)
 
 #response = query_engine.query(question)
 #print(response)
